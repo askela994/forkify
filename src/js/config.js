60d2a2476b0c979333e55f9595cab2081d9b608a
askela994 <letesamobombe824@gmail.com>
2022-05-26 11:46:10 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '4253cc64-77e9-44eb-b35c-5b135bfd8ecd';
+export const MODAL_CLOSE_SEC = 2.5;
